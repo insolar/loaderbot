22abfd1ca54208afbe6ae35f635f4d83b1ee9937
Sergey Kudasov <sergey.kudasov@insolar.io>
2020-07-20 17:54:27 +0300

    scaling report

--- go.mod
+++ go.mod
@@ -3,10 +3,12 @@ module github.com/insolar/loaderbot
 go 1.14
 
 require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/ory/go-acc v0.2.3 // indirect
-	github.com/spf13/viper v1.7.0
 	github.com/streadway/quantile v0.0.0-20150917103942-b0c588724d25
 	github.com/stretchr/testify v1.6.1
+	github.com/wcharczuk/go-chart v2.0.2-0.20191206192251-962b9abdec2b+incompatible
 	go.uber.org/ratelimit v0.1.0
 	go.uber.org/zap v1.10.0
+	golang.org/x/image v0.0.0-20200618115811-c13761719519 // indirect
 )
---
+++ scaling_report.go
@@ -0,0 +1,110 @@
+package loaderbot
+
+import (
+	"encoding/csv"
+	"github.com/wcharczuk/go-chart"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
+type ChartLine struct {
+	XValues []float64
+	YValues []float64
+}
+
+func ReadCsvFile(path string) (map[string]ChartLine, error) {
+	csvFile, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer csvFile.Close()
+	reader := csv.NewReader(csvFile)
+	requests := make(map[string]ChartLine)
+
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		if len(record) != 3 {
+			return nil, err
+		}
+
+		methodName := record[0]
+		veCount := record[1]
+		maxRPS := record[2]
+
+		line := requests[methodName]
+		xValue, err := strconv.ParseFloat(veCount, 64)
+		if err != nil {
+			return nil, err
+		}
+		// VE count
+		line.XValues = append(line.XValues, xValue)
+		yValue, err := strconv.ParseFloat(maxRPS, 64)
+		if err != nil {
+			return nil, err
+		}
+		// Max RPS
+		line.YValues = append(line.YValues, yValue)
+		requests[methodName] = line
+	}
+	return requests, nil
+}
+
+func ReportScaling(inputCsv, outputPng string) {
+	lines, err := ReadCsvFile(inputCsv)
+	if err != nil {
+		log.Fatal("Couldn't read and parse requests", err)
+	}
+
+	err = RenderChart(lines, outputPng)
+	if err != nil {
+		log.Fatal("Couldn't render chart", err)
+	}
+}
+
+func RenderChart(requests map[string]ChartLine, fileName string) error {
+	var series []chart.Series
+	var colorIndex int
+	for key, value := range requests {
+		series = append(series, chart.ContinuousSeries{
+			Name: key,
+			Style: chart.Style{
+				StrokeColor: chart.GetDefaultColor(colorIndex).WithAlpha(255),
+				StrokeWidth: 5,
+			},
+			XValues: value.XValues,
+			YValues: value.YValues,
+		})
+		colorIndex++
+	}
+
+	graph := chart.Chart{
+		XAxis: chart.XAxis{
+			Name: "VE count",
+		},
+		YAxis: chart.YAxis{
+			Name: "Max RPS",
+		},
+		Series: series,
+	}
+
+	graph.Elements = []chart.Renderable{
+		chart.Legend(&graph),
+	}
+
+	file, err := os.Create(fileName)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	graph.Render(chart.PNG, file)
+	return graph.Render(chart.PNG, file)
+}
